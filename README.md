b1787671c24dba8c7bf9313dab04e8c4fb1188b7
Proudmuslim <proudmuslimos@protonmail.ch>
2020-10-05 07:45:04 -0700

    Add message deletion logging (edit logging is not working atm; going to fix that right now) and a proper README

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# Overseer Discord Bot
-Overseer is an open source discord bot, meant for moderation purposes.
-Full readme coming soon
+# Overseer  
+#### ![Overseer](https://cdn.discordapp.com/avatars/717215043724509275/a82627ad58710534531cec935abd5e87.webp?size=512 "Overseer")
+
+Overseer is a multi purpose, open source discord bot, developed primarily by 𝓟𝓻𝓸𝓾𝓭𝓶𝓾𝓼𝓵𝓲𝓶#5818. I made this bot as a sort of side project, and as something for other people to base their code off of. 
+- The bot is still very much a work in progress, and contributions from others are more than welcome.
+- I'm working on getting a proper todo list on this repo, but that will take a bit as I don't have anything planned out properly yet.
+- Be sure to follow the license when forking/working on the project
+
+## Requirements:
+
+- Discordpy (latest)
+- Aiohttp
+- Termcolor
+- Jishaku 
+
+All of these can be installed using `pip`.
--- cogs/logging.py
+++ cogs/logging.py
@@ -1,8 +1,10 @@
 import discord
 from discord.ext import commands
+from datetime import datetime
 
 class Logging(commands.Cog):
     """Logging Commands"""
+
     def __init__(self, client):
         self.client = client
 
@@ -10,10 +12,39 @@ class Logging(commands.Cog):
     async def logging(self, ctx):
         await ctx.send(":warning: Logging commands are currently under development.")
 
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        if message.guild.id in [743073728853835828]:
+            channel = discord.utils.get(message.guild.channels, name="logs")
+            embed = discord.Embed(color=discord.Colour.from_rgb(255, 150, 53))
+            embed.add_field(name=f"Message by {message.author} deleted in #{message.channel.name}",
+                            value=message.content)
+            embed.set_footer(
+                icon_url="https://cdn.discordapp.com/avatars/573986854366347274/76b36e11e0757464a6477f480bf5f543.webp?size=1024",
+                text="Developed by 𝓟𝓻𝓸𝓾𝓭𝓶𝓾𝓼𝓵𝓲𝓶#5818")
+            await channel.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        channel = discord.utils.get(message.guild.channels, name="logs")
+        if before.content != after.content:
+            embed = discord.Embed(title="Message edit",
+                                    description=f"Edit by {after.author.display_name}.",
+                                    colour=after.author.colour,
+                                    timestamp=datetime.utcnow())
+
+            fields = [("Before", before.content, False),
+                          ("After", after.content, False)]
+
+            for name, value, inline in fields:
+            embed.add_field(name=name, value=value, inline=inline)
+
+            await channel.send(embed=embed)
+
+
 def setup(client):
-	client.add_cog(Logging(client))
+    client.add_cog(Logging(client))
 
 
 def teardown(client):
-	client.remove_cog(Logging.__name__)
-
+    client.remove_cog(Logging.__name__)
